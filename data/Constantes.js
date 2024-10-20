27e2b1ca47a50594ffad81ba8353feb0aedcea25
Matias Imoff <matias.c.imoff@gmail.com>
2024-10-20 11:51:25 -0300

    Conexion a MySQL concretada

---
+++ data/Constantes.js
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+
+export const CREDENCIALES = {
+    USER: process.env.USER,
+    PASSWORD: process.env.PASSWORD
+}
--- db.js
+++ db.js
@@ -1,10 +1,13 @@
+import {CREDENCIALES}  from './data/Constantes.js'
+
 const mysql = require('mysql2')
+
 
 
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'Matias',
-    password: 'Matias77**',
+    user: `${CREDENCIALES.NAME}`,
+    password: `${CREDENCIALES.PASSWORD}`,
     database: 'logistics'
 
 });
@@ -15,5 +18,38 @@ connection.connect((err) => {
     }
     console.log('Conexión a la base de datos MySQL exitosa.');
 });
+
+//Agregar un nuevo producto a la base de datos
+
+const articuloNuevo = `INSERT INTO logistics.stock (id, name, quantity, price) VALUES (NULL, 'Lentejas', '100', '30');`
+
+connection.query(articuloNuevo,function(error,articulo){
+    if(error){
+        throw error;
+        
+    }else{
+        console.log("Datos ingresados correctamente")
+    }
+    
+})
+
+//Consultar la base de datos completa
+
+const articulos = "SELECT * FROM logistics.stock"
+
+connection.query(articulos,function(error,lista){
+    if(error){
+        throw error;
+        
+    }else{
+        console.log(lista[lista.length -1])
+    }
+    
+})
+
+
+
+
+connection.end()
 
 
